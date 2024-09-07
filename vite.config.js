ac9fbc4d19ee927b44d524a016369dfafc5f864b
C.Gunasekar <137645060+guna-design@users.noreply.github.com>
2024-09-08 04:53:27 +0530

    source hide

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import obfuscator from 'vite-plugin-obfuscator';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    obfuscator({ /* options here */})
+  ], build: {
+    sourcemap: false  // Disable source maps for production
+  }
 })
