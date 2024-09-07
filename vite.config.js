f7d12f61e42b42cfdd8041040b7675251b904429
C.Gunasekar <137645060+guna-design@users.noreply.github.com>
2024-09-08 05:03:32 +0530

    rrestored

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import obfuscator from 'vite-plugin-obfuscator';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    obfuscator({ /* options for obfuscation */ }) // Ensure this is correctly configured
-  ],
-  build: {
-    sourcemap: false  // Disable source maps in production
-  }
-});
+  plugins: [react()],
+})
